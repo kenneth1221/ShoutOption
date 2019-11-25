107b283fbcdffd30208227574a7e6b59056a6b95
Steven Xu <stevenxu@att.net>
2019-11-25 15:19:01 -0500

    Working implementation of Jeff's code on my machine

--- jeff_functions.py
+++ jeff_functions.py
@@ -9,7 +9,7 @@ import numpy as np
 import pandas as pd
 from pandas_datareader import data
 import datetime as dt
-
+import os
 
 import scipy.stats as si
 def euro_vanilla_call(S, K, T, r, sigma):
@@ -30,8 +30,11 @@ s=3128.65
 T_t = 1
 r = 0.0158
 sigma = 0.1
-#%%
+
+os.chdir(os.path.dirname(__file__))
+print(os.getcwd())
 options = pd.read_excel("options.xlsx")
+#options = pd.read_csv("options.csv")
 c = options.iloc[:, 1].values # traded call options price
 K = options.iloc[:, 0].values/100 # strike price
 
