bf873f2c059288626ef152770eaad3f74a968fd5
Steven Xu <stevenxu@att.net>
2019-12-07 20:41:45 -0500

    more development, thoughts on how to do the damn regression

--- ShoutOption.py
+++ ShoutOption.py
@@ -37,7 +37,6 @@ def euro_vanilla_call(S, K, T, r, d, sigma):
     #sigma: volatility of underlying asset
     d1 = (np.log(S / K) + (r - d + 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
     d2 = (np.log(S / K) + (r - d - 0.5 * sigma ** 2) * T) / (sigma * np.sqrt(T))
-    
     call = (S * si.norm.cdf(d1, 0.0, 1.0) - K * np.exp(-(r-d) * T) * si.norm.cdf(d2, 0.0, 1.0))
     
     return call
@@ -78,7 +77,7 @@ yearlyalpha = dailyalpha*daybasis
 
 #%%
 # generate  set of random numbers
-num_random = 10000
+num_random = 5000
 Z1 = rd.randn(num_random,1)
 Z2 = rd.randn(num_random,1)
 Z1 = (Z1-Z1.mean())/Z1.std()
@@ -91,7 +90,7 @@ def RegenerateRandomNumbers():
     Z1 = (Z1-Z1.mean())/Z1.std()
     Z2 = (Z2-Z2.mean())/Z2.std()
     
-def TriggerPayoff(Q):
+def TriggerPayoff(Q, F):
     """ calculates trigger payoff for a given exercise level Q via Monte Carlo simulation"""
     
     Shalf = S*np.exp( ( r - d - sigma**2/2)*trig + sigma*Z1 * np.sqrt(trig)) 
@@ -126,16 +125,16 @@ options = pd.read_excel("options.xlsx")
 #options = pd.read_csv("options.csv")
 c1yr = options.loc[:, 'Dec 2020 call'].values # traded call options price
 chalfyr = options.loc[:, 'Jun 2020 call'].values
-K = options.loc[:, 'strike'].values/100 # strike price
+Ks = options.loc[:, 'strike'].values/100 # strike price
 # suppose maturity = 1 
 bs_c = []
 
-for i in K:
+for i in Ks:
     bs_c.append(euro_vanilla_call(S, i, T, r, d,sigma))
     
 bs_c = np.asarray(bs_c, dtype=np.float64).reshape(-1,1)
 #Xs is a matrix of theoretical black scholes price and strike. 
-Xs = np.hstack((bs_c, K.reshape(-1,1)))
+Xs = np.hstack((bs_c, Ks.reshape(-1,1)))
 #%%
 #linear regression to connect the black scholes call price to the 1 year call price
 # establishes a link between the theoretical black scholes and market observed prices
@@ -143,44 +142,58 @@ Xs = np.hstack((bs_c, K.reshape(-1,1)))
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
 regressor = LinearRegression()
-regressorb = LinearRegression()
+#regressorb = LinearRegression()
 model_1 = regressor.fit(bs_c, c1yr)
 
-print('slope: ',model_1.coef_)
-print('intercept: ',model_1.intercept_)
-print(r2_score(model_1.coef_*bs_c+model_1.intercept_, c1yr))
+b_year, a_year = model_1.coef_[0], model_1.intercept_
 
-multlin_1 = regressorb.fit(Xs,c1yr)
-print(multlin_1.coef_)
-print(multlin_1.intercept_)
-print(r2_score( np.sum(multlin_1.coef_*Xs, axis = 1)+multlin_1.intercept_, c1yr))
+print('slope: ',b_year)
+print('intercept: ',a_year)
+print('r2 score: ',r2_score(model_1.coef_*bs_c+model_1.intercept_, c1yr))
 
-plt.plot(K, model_1.coef_*bs_c+model_1.intercept_, '.')
-plt.plot(K, np.sum(multlin_1.coef_*Xs, axis = 1)+multlin_1.intercept_, '.')
-plt.plot(K,c1yr, '.')
+#multlin_1 = regressorb.fit(Xs,c1yr)
+#print(multlin_1.coef_)
+#print(multlin_1.intercept_)
+#print(r2_score( np.sum(multlin_1.coef_*Xs, axis = 1)+multlin_1.intercept_, c1yr))
+
+plt.plot(Ks, model_1.coef_*bs_c+model_1.intercept_, '.')
+#plt.plot(Ks, np.sum(multlin_1.coef_*Xs, axis = 1)+multlin_1.intercept_, '.')
+plt.plot(Ks,c1yr, '.')
 
 
 #%%
 # suppose maturity = 0.5 
 # regresses half year black scholes theoretical call price to real call price
-
 bs_c2 = []
-T_t = 0.5
-for i in K:
-    bs_c2.append(euro_vanilla_call(s, i, T_t, r, d,sigma))
+for i in Ks:
+    bs_c2.append(euro_vanilla_call(S, i, trig, r, d,sigma))
     
 bs_c2 = np.asarray(bs_c2, dtype=np.float64).reshape(-1,1)
 
 regressor2 = LinearRegression()
 model_2 = regressor2.fit(bs_c2, chalfyr)
 
-print(model_2.coef_)
-print(model_2.intercept_)
-print(r2_score(model_2.coef_*bs_c2+model_2.intercept_, chalfyr))
+b_half, a_half = model_2.coef_[0], model_2.intercept_
+print('slope: ', b_half)
+print('intercept: ', a_half)
+print('r2 score: ',r2_score(model_2.coef_*bs_c2+model_2.intercept_, chalfyr))
 
-plt.plot(K, model_2.coef_*bs_c2+model_2.intercept_, '.')
-plt.plot(K, chalfyr, '.')
+plt.plot(Ks, model_2.coef_*bs_c2+model_2.intercept_, '.')
+plt.plot(Ks, chalfyr, '.')
+#%% 
+# these coefficients relate black scholes prices to market observed prices for a given strike
+# for strikes between  3075 and 3150, for options of 1 year and half a year
+# of course, this might not be necessary -- it might be simpler to get straight to the point
+# which means regress directly: (what to what?)
+# simulate shout values, let those be Y
+# xs are then... 
+# I guess it could be with different strike prices to empirically determine a payoff function for a shout
+# theoretically with a shout F of 0, the damn thing should converge to a european call
+print(a_year, b_year, a_half, b_half)
 
+def scaled_eurocall(bsprice, alpha, beta):
+    return bsprice*beta + alpha
+    
 #%%
 def main(k):
     sigma = yearlyvol
@@ -189,10 +202,11 @@ def main(k):
     r = .0158
     d = .0185
     S = SP500.iloc[-1].Close
-    F = 10
+    F = 100
     K = k
     RegenerateRandomNumbers()
     payoffs = []
+    basepays = []
     eurocall = []
     trueeurocall = []
     strikes = []
@@ -202,8 +216,8 @@ def main(k):
     step = .2
     steprange = np.arange(minrange, maxrange, step)
     
-    simeurcall = TwoPeriodEuroCall()
-    bseurcall = euro_vanilla_call(S,K,T,r,d,sigma)
+    simeurcall = scaled_eurocall(TwoPeriodEuroCall(),a_year, b_year )#common random number eurocall
+    bseurcall = scaled_eurocall(euro_vanilla_call(S,K,T,r,d,sigma), a_year, b_year )#analytical eurocall
     
     for i in steprange:
         # this payoff takes the control variate technique and applies it to the trigger payoff
@@ -211,9 +225,11 @@ def main(k):
         # we can simulate the european call price using the same common random numbers as the trigger
         # thus, we can correct the effect of the randomnuess on the trigger payoff via:
         # simulated_trigger - simulated_european + analytical_european
-        
-        j = TriggerPayoff(i) - simeurcall + bseurcall
+        basepay = TriggerPayoff(i,F)
+
+        j = basepay - simeurcall + bseurcall
         payoffs.append(j)
+        basepays.append(basepay)
         eurocall.append(simeurcall)
         trueeurocall.append(bseurcall)
         strikes.append(i)
@@ -226,10 +242,10 @@ def main(k):
     return value, bestq
 
 #%%
-k = 3110
+k = 3100
 values = []
 optimalqs = []
-for j in range(10):        
+for j in range(5):        
     v, q = main(k)
     values.append(v)
     optimalqs.append(q)
